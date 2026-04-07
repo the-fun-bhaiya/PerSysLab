5f3f5a7640609d44868e1fae2c7194933b74356c
funBhaiya <khushwantchhipa21@gmail.com>
2026-04-07 21:14:33 +0530

    enhanced error handler for apis that expects json

--- src/public/js/form/form.js
+++ src/public/js/form/form.js
@@ -14,47 +14,48 @@
       },
       false,
     );
-  }); 
-  function togglePassword() {
-    const passwordInput = document.getElementById("password");
-    const toggleIcon = document.getElementById("toggleIcon");
+  });
+  const passwordInput = document.getElementById("password");
+  const toggleIcon = document.getElementById("toggleIcon");
 
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text";
-      toggleIcon.classList.replace("fa-eye", "fa-eye-slash");
-    } else {
-      passwordInput.type = "password";
-      toggleIcon.classList.replace("fa-eye-slash", "fa-eye");
-    }
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    toggleIcon.classList.replace("fa-eye", "fa-eye-slash");
+  } else {
+    passwordInput.type = "password";
+    toggleIcon.classList.replace("fa-eye-slash", "fa-eye");
   }
   const submitBtn = document.querySelector("#submitBtn");
-  document.querySelector("#form").addEventListener("keypress", async (event) => {
-    event.key == "Enter"
-      ? submitBtn.click()
-      : console.log("you're typing. hehehe...😏");
-  });
- 
-    submitBtn.addEventListener("click", async (event) => {
-      console.log("clicked and blocked");
-      event.target.disabled = true;
-      const username = document.querySelector("#username").value;
-      const password = document.querySelector("#password").value;
-      const data = { username, password };
-      console.dir(data);
-      const endpoint = submitBtn.dataset.endpoint
-      const res = await fetch(endpoint, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data),
-      });
-      console.log(JSON.stringify(data));
-      
-      if (res) event.target.disabled = false;
-
-      const resData = await res.json();
-      if (typeof resData.redirectedUrl !== "undefined") window.location.assign(resData.redirectedUrl)
-      else showPSLToast(resData.msgType, resData.msg)
+  document
+    .querySelector("#form")
+    .addEventListener("keypress", async (event) => {
+      event.key == "Enter"
+        ? submitBtn.click()
+        : console.log("you're typing. hehehe...😏");
     });
+
+  submitBtn.addEventListener("click", async (event) => {
+    console.log("clicked and blocked");
+    event.target.disabled = true;
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+    const data = { username, password };
+    console.dir(data);
+    const endpoint = submitBtn.dataset.endpoint;
+    const res = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    console.log(JSON.stringify(data));
+
+    if (res) event.target.disabled = false;
+
+    const resData = await res.json();
+    if (typeof resData.redirectedUrl !== "undefined")
+      window.location.assign(resData.redirectedUrl);
+    else showPSLToast(resData.msgType, resData.msg);
+  });
 })();
