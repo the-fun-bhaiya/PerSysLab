a37bfe716ab2722f4c6b426fabe988561a1060fb
funBhaiya <khushwantchhipa21@gmail.com>
2026-04-07 21:10:54 +0530

    enhanced error handler for apis that expects json

--- utils/customError/errHandler.js
+++ utils/customError/errHandler.js
@@ -1,47 +1,30 @@
-import dotenv from  "dotenv";
-import path, {dirname} from "path";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-dotenv.config({ path: path.join(__dirname, "..", "..", "src", ".env") });
-
 export const globalErr = (err, req, res, next) => {
   const def = {
-    msg: "We tried our best to keep up this flight. but sorry... we crashed. :(",
-    link: "/",
-    btn: "Home",
+    msg: "Something went wrong.",
+    btnLink: "/",
+    btnName: "Home",
   };
-  const isProduction = process.env.NODE_ENV === "production"
+
+  const isProduction = process.env.NODE_ENV === "production";
+
   if (!isProduction) {
     console.log(err.stack);
-    if (req.xhr || req.headers.accept?.includes("application/json")) {
-      return res.status(err.statusCode || 500).json({
-        success: false,
-        message: err.msg || def.msg,
-      });
-    }
-    const msgObj = {
-      msg: def.msg,
-      btnLink: def.link,
-      btnName: def.btn,
-      title: "Uh..Ohhh",
-      css: `err/errPage`,
-    };
-    res.render("errPage/errPage", msgObj);
-    return;
   }
+
   if (req.xhr || req.headers.accept?.includes("application/json")) {
     return res.status(err.statusCode || 500).json({
       success: false,
       message: err.msg || def.msg,
     });
   }
+
   const msgObj = {
-    msg: err.msg,
-    btnLink: err.btnLink,
-    btnName: err.btnName,
+    msg: isProduction ? err.msg : def.msg,
+    btnLink: err.btnLink || def.btnLink,
+    btnName: err.btnName || def.btnName,
     title: "Uh..Ohhh",
-    css: `err/errPage`
+    css: `err/errPage`,
   };
-  res.render("errPage/errPage", msgObj);
+
+  res.status(err.statusCode || 500).render("errPage/errPage", msgObj);
 };
